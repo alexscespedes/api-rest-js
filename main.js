f539ac9087f2f42d83d9e33fc02b49162ee6ce6f
alexsencion <alexandersenc99@gmail.com>
2023-11-03 12:48:44 -0400

    Add get random-favorites

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=5&api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
+const URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
+
+const URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
+
 
 // fetch(URL)
 //     .then(response => response.json())
@@ -10,23 +13,40 @@ const URL = 'https://api.thecatapi.com/v1/images/search?limit=5&api_key=live_eUG
 // RETO COMPLETADO!!!
 
 const button = document.querySelector('#btn')
+const spanError = document.getElementById('error')
 
-button.addEventListener("click", obtenerImagenDesdeURL)
-
-    async function obtenerImagenDesdeURL() {
+    async function loadRandomCats() {
         try {
-            const response = await fetch(URL);
+            const response = await fetch(URL_RANDOM);
             const data = await response.json();
             console.log(data);
+            console.log("Random")
             const img1 = document.getElementById('img1');
             img1.src = data[0].url;
             const img2 = document.getElementById('img2');
             img2.src = data[1].url;
-            const img3 = document.getElementById('img3');
-            img3.src = data[2].url;
         } catch (error) {
             console.error('Error al obtener y mostrar la imagen:', error);
+            spanError.innerHTML = 'Hubo un error al cargar las imagenes';
         }
     }
 
-obtenerImagenDesdeURL()
+    
+
+    async function loadFavoritesCats() {
+        try {
+            const response = await fetch(URL_FAVORITES);
+            const data = await response.json();
+            console.log(data);
+            console.log("Favorites")
+
+
+        } catch (error) {
+            console.error('Error al obtener y mostrar la imagen:', error);
+            spanError.innerHTML = 'Hubo un error al cargar las imagenes';
+        }
+    }
+
+
+    loadRandomCats()
+    loadFavoritesCats()
