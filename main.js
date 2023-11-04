4db7307501bc2d82fe98c14c48d03e2bf28c6624
alexsencion <alexandersenc99@gmail.com>
2023-11-04 18:53:22 -0400

    Add get, post & delete methods

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 const URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
 
-const URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
+const URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y';
+const URL_DELETE_FAVORITES = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_eUG5kBaW9Zax1TFTuSkshtUaSL7MVGl2VWWWCYDYIkbooD492jj74V59dEHWya3Y`;
 
 
-// fetch(URL)
-//     .then(response => response.json())
-//     .then(data => {
-//         const img = document.querySelector('img')
-//         img.src = data[0].url;
-//     });
-
-// RETO COMPLETADO!!!
-
-const button = document.querySelector('#btn')
 const spanError = document.getElementById('error')
 
     async function loadRandomCats() {
@@ -22,9 +13,25 @@ const spanError = document.getElementById('error')
             console.log(data);
             console.log("Random")
             const img1 = document.getElementById('img1');
-            img1.src = data[0].url;
             const img2 = document.getElementById('img2');
+            const btn1 = document.getElementById('btn1');
+            const btn2 = document.getElementById('btn2');
+            
+
+            img1.src = data[0].url;
             img2.src = data[1].url;
+            btn1.addEventListener('click', () => {
+                saveFavoriteCats(data[0].id);
+            })
+
+            btn2.addEventListener('click', () => {
+                saveFavoriteCats(data[1].id);
+            })
+
+
+
+
+
         } catch (error) {
             console.error('Error al obtener y mostrar la imagen:', error);
             spanError.innerHTML = 'Hubo un error al cargar las imagenes';
@@ -38,7 +45,32 @@ const spanError = document.getElementById('error')
             const response = await fetch(URL_FAVORITES);
             const data = await response.json();
             console.log(data);
-            console.log("Favorites")
+            const section = document.getElementById('favoriteCats');
+            section.innerHTML = "";
+
+            const h2 = document.createElement('h2');
+            const h2Text = document.createTextNode('Michis favoritos');
+            h2.appendChild(h2Text);
+            section.appendChild(h2);
+
+            data.forEach(element => {
+                
+                const article = document.createElement('article');
+                const img = document.createElement('img');
+                const btn = document.createElement('button');
+                const btnText = document.createTextNode('Remove cat from favorites');
+
+                img.src = element.image.url;
+                img.width = 200;
+                btn.appendChild(btnText);
+                btn.addEventListener('click', () => {
+                    deleteFavoriteCats(element.id);
+                })
+                article.appendChild(img);
+                article.appendChild(btn);
+                section.appendChild(article);
+
+            });
 
 
         } catch (error) {
@@ -46,6 +78,50 @@ const spanError = document.getElementById('error')
             spanError.innerHTML = 'Hubo un error al cargar las imagenes';
         }
     }
+
+    async function saveFavoriteCats(id) {
+        try {
+            const response = await fetch(URL_FAVORITES, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    image_id: id
+                }),
+            });
+            const data = await response.json();
+            console.log(data);
+            console.log("Cat successfully saved")
+            loadFavoritesCats()
+
+
+        } catch (error) {
+            console.error('Error al guardar la imagen:', error);
+            spanError.innerHTML = 'Hubo un error al guardar la imagen';
+        }
+    }
+
+    async function deleteFavoriteCats(id) {
+        try {
+            const response = await fetch(URL_DELETE_FAVORITES(id), {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+            console.log(data);
+            console.log("Cat successfully removed")
+            await loadFavoritesCats()
+
+
+        } catch (error) {
+            console.error('Error al guardar la imagen:', error);
+            spanError.innerHTML = 'Hubo un error al guardar la imagen';
+        }
+    }
+
 
 
     loadRandomCats()
